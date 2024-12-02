5fd91c3c0a64577926dbb1ce6ec03c79665a36bd
Carlos Guaman <ca.99gs@gmail.com>
2024-12-02 18:08:43 -0500

    Primera subida

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reloj Digital</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="clock">
+        <div id="time"></div>
+    </div>
+
+    <script src="{{ asset('js/script.js') }}"></script>
+</body>
+</html>
